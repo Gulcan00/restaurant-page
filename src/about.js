c9c9a41733410a5547c1e0442b6c72e855ef9fe9
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-11-28 22:16:11 +0200

    TO do: who we are section

---
+++ src/about.js
@@ -0,0 +1,20 @@
+/**
+ * WHo we are
+ * ## Who We Are
+
+- **Remy**: Head Chef - The mastermind behind our exquisite dishes, Remy brings passion and innovation to our kitchen.
+  
+- **Linguini**: Sous Chef - Linguini assists Remy in the kitchen, his enthusiasm and dedication are key to our culinary success.
+
+- **Colette**: Chef de Partie - Colette's expertise and attention to detail ensure that every dish is prepared to perfection.
+
+- **Gusteau**: Inspirational Guide - Although not with us in person, the spirit of the great Chef Gusteau guides us in our culinary journey, reminding us that "anyone can cook".
+
+- **Skinner**: Restaurant Manager - Skinner ensures the smooth operation of La Ratatouille, overseeing everything from service to logistics.
+
+- **Anton Ego**: Loyal Patron - Our harshest critic turned biggest supporter, Anton Ego's reviews have helped shape La Ratatouille into the culinary gem it is today.
+ */
+
+export default function createAbout() {
+    
+}
