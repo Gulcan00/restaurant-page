63a3e4418be7fc162ec5fca5263982186aaf64e4
Gulcan Damdelen <61693319+Gulcan00@users.noreply.github.com>
2023-11-28 22:14:43 +0200

    To do menu items

---
+++ src/menu.js
@@ -0,0 +1,31 @@
+/*
+MENU ITEMS
+## La Ratatouille Menu
+
+### Entrées
+1. **Soupe à l'oignon** - Classic French onion soup topped with melted Gruyère cheese.
+2. **Escargots de Bourgogne** - Snails cooked in a tantalizing garlic-herb butter.
+3. **Salade Niçoise** - Fresh salad with tuna, hard-boiled eggs, Niçoise olives, and anchovies.
+
+### Plats Principaux
+1. **Ratatouille** - Our signature dish, a traditional Provençal vegetable stew, lovingly prepared by our Little Chef, Remy.
+2. **Coq au Vin** - Chicken slow-cooked in a rich red wine sauce with mushrooms and pearl onions.
+3. **Bouillabaisse** - A hearty seafood stew, brimming with the freshest catch of the day and served with a side of rouille sauce.
+
+### Desserts
+1. **Crème Brûlée** - Creamy custard base topped with a contrasting layer of hard caramel.
+2. **Tarte Tatin** - Upside-down caramelized apple tart served warm.
+3. **Mousse au Chocolat** - Rich and creamy chocolate mousse, a perfect end to your meal.
+
+### Fromages
+1. **Assiette de Fromages** - A selection of the finest French cheeses.
+
+### Boissons
+1. A selection of French wines, coffee, and soft drinks.
+
+Please note: Our menu changes with the seasons to ensure we use the freshest ingredients. Bon Appétit!
+*/
+
+export function createMenu() {
+
+}
